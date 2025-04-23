ca286283120d69b06ce62d665486db2bcb6346cf
harriscw <harriscw@ucla.edu>
2025-04-23 08:58:16 -0700

    better per game filtering, widgets are global now

--- SingleA/app.R
+++ SingleA/app.R
@@ -21,44 +21,53 @@ server <- function(input, output) {
   # read data
   df_=readxl::read_excel("data/A Ball Scouting Reports.xlsx")
   
-  # reactively filter to team
-  df = reactive({
-    print(input$team)
 
+  df = reactive({
+
+    # reactively filter to team
     if(input$team != "All"){
       df_=df_ %>% filter(Team==input$team)
     }
-    df_
-    })
-  
-  # set game filter
-  thenum=reactive({
-    if(input$radio=="all"){
-      0
-    }else if(input$radio=="second half"){
-      8
+    
+    # reactively filter games
+    if(input$radio=="second half"){
+      df_ = df_ %>% filter(Game>8)
     }else if(input$radio=="last 3"){
-      max(df()$Game)-3
+      df_ = df_ %>% 
+        group_by(Name) %>% 
+        dplyr::slice(tail(row_number(),3)) %>%
+        ungroup()
     }else if(input$radio=="last 6"){
-      max(df()$Game)-6
+      df_ = df_ %>% 
+        group_by(Name) %>% 
+        dplyr::slice(tail(row_number(),6)) %>% 
+        ungroup()
     }
     
-  })
+    df_
+    
+    })
+  
+
   
   # display for aggregate data
   output$agg = renderDT({
     
+    # df=df_ %>% filter(Team=="Lugnuts")
+    
     df() %>% 
-      filter(Game>thenum()) %>%
       group_by(Team,Name) %>% 
       summarize(games=n(),
                 order=round(mean(Order),2),
                 Hits=sum(Hits),
                 AB=sum(`At Bats`),
                 XBH=sum(XBH),
-                SO=sum(SO)) %>% 
+                SO=sum(SO)
+                ) %>% 
       ungroup() %>% 
-      mutate(AVG=round(Hits/AB,3)) %>% 
+      mutate(AVG=round(Hits/AB,3),
+             `SO/G`=round(SO/games,2)
+             ) %>% 
       arrange(desc(AVG)) %>% 
       mutate(`Percentile (AVG)`=round(100*(nrow(.)-row_number())/nrow(.),2))
     
@@ -95,20 +104,19 @@ server <- function(input, output) {
           selectInput("team", 
                       label = "Team", 
                       choices = c("All",unique(df_$Team)), 
-                      selected = "All")
+                      selected = "All"),
+          radioButtons("radio", label = "Filter",
+                       choices = list("All Games" = "all", 
+                                      "Second Half" = "second half", 
+                                      "Last 3" = "last 3",
+                                      "Last 6" = "last 6"
+                       ), 
+                       selected = "all")
           
         ),
         mainPanel(
         tabsetPanel(
           tabPanel("Aggregate",
-                   
-                   radioButtons("radio", label = "Filter",
-                                choices = list("All Games" = "all", 
-                                               "Second Half" = "second half", 
-                                               "Last 3" = "last 3",
-                                               "Last 6" = "last 6"
-                                ), 
-                                selected = "all"),
                    DTOutput('agg')
                    
           ),
